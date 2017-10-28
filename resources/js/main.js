410386c4496bc0381ff5c7536d90dabf4f29b290
Kaja89 <kajaspiro@gmail.com>
2017-10-28 22:45:36 +0200

    sketch finished

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,5 +3,29 @@ $('.slider').slick({
   infinite: true,
   speed: 300,
   slidesToShow: 3,
-  adaptiveHeight: true
+  adaptiveHeight: true,
+
+responsive: [
+
+   {
+     breakpoint: 1150,
+     settings: {
+       slidesToShow: 2,
+       slidesToScroll: 1
+     }
+   },
+
+   {
+     breakpoint: 800,
+     settings: {
+       slidesToShow: 1,
+       slidesToScroll: 1,
+       arrows: false
+     }
+   }
+
+   // You can unslick at a given breakpoint now by adding:
+   // settings: "unslick"
+   // instead of a settings object
+ ]
 });
